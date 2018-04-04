9bb7ff5458b1e75641604505876cfb63688df45d
tbui72201 <tbui72201@users.noreply.github.com>
2018-04-03 21:49:39 -0500

    Update design.js

--- design.js
+++ design.js
@@ -1,8 +1,37 @@
-// Select color input
-// Select size input
+/* Defining grids - default value is 10 min and 250 max for width and height */
 
-// When size is submitted by the user, call makeGrid()
+var gridColor = document.getElementById('colorPicker');
+var gridHeight = document.getElementById('inputHeight');
+var gridWidth = document.getElementById('inputWeight');
+var gridtable = document.getElementById('pixelCanvas');
+  
+/* Function - makeGrid() */ 
 
-function makeGrid() {
-
+document.addEventListener('submit',function makeGrid(e){
+/* prevent browser auto reload */ 
+e.preventDefault();
+gridtable.innerHTML = '';
+    
+function clickable(gridCell) {
+  gridCell.style.background = gridColor.value;
 }
+
+/* Function - setup for loop for adding new cells and rows for grid*/     
+    
+    for(var h=1;h<=gridHeight.value;h++){
+    var row=document.createElement('tr');
+    for(var w=1;w<=gridWidth.value;w++){
+    var cell=document.createElement('td');
+    cell.onclick=function () {
+            clickable(this);
+        };
+/* adding new cell */
+    row.append(cell);
+
+      }
+/* adding new row into table to make grid */
+    gridtable.append(row);
+  }
+  makeGrid();
+    
+});
